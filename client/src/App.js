4ca2fbbe24147a6235cc4752ac5a53387aa238c7
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-06-27 23:13:22 +0500

    fix: заменён BrowserRouter на HashRouter для стабильного роутинга в WebApp/Vercel

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import LoadingScreen from './components/LoadingScreen';
 import { api } from './services/api';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Tasks from './pages/Tasks';
 import Profile from './pages/Profile';
