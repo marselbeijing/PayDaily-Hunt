e976a144ebe471bba81dbfae62a3af8421f23865
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-07-02 22:33:17 +0500

    Увеличил отступы слайдера до 8% для лучшего центрирования

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -57,7 +57,8 @@ export default function Home({ onNavigate }) {
           spaceBetween={16}
           slidesPerView={1.1}
           centeredSlides={true}
-          className="rounded-2xl"
+          className="rounded-2xl mx-auto"
+          style={{ paddingLeft: '8%', paddingRight: '8%' }}
           autoplay={{ delay: SLIDE_DURATION, disableOnInteraction: false }}
           loop={true}
           modules={[Autoplay]}
